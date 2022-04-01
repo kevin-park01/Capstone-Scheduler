5705f39ef3a2f40c3b599fc5d107b73d56689f06
kevin-park01 <jkphyke08@gmail.com>
2022-03-31 19:26:52 -0700

    Functions for Filtered Sessions and Rooms

--- controller.py
+++ controller.py
@@ -3,43 +3,62 @@ from flask import Flask, render_template
 from datetime import datetime
 
 
-room1 = schedule.Room(1, 10)
-room2 = schedule.Room(2, 10)
+room1 = schedule.Room(1, 10, 'Room 1', 'WSCC', 1)
+room2 = schedule.Room(2, 10, 'Room 2', 'WSCC', 1)
 
-session1 = schedule.Session(1, 75, 5, 'Intro to CS 1', 'Roundtable', 'CS', 'Social Event', ['Mic'], [1])
-session2 = schedule.Session(2, 75, 5, 'Intro to CS 2', 'Roundtable', 'CS', 'Social Event', ['Mic', 'Wifi'], [1])
-session3 = schedule.Session(3, 75, 5, 'Intro to BS 1', 'Roundtable', 'BS', 'Social Event', ['Mic'], [2])
-session4 = schedule.Session(4, 75, 5, 'Intro to BS 2', 'Roundtable', 'BS', 'Social Event', ['Wifi'], [2])
-session5 = schedule.Session(5, 75, 5, 'Intro to ENG 1', 'Roundtable', 'BS', 'Social Event', ['Wifi'], [2])
-session6 = schedule.Session(6, 75, 5, 'Intro to ENG 2', 'Roundtable', 'BS', 'Social Event', ['Wifi'], [2])
+session1 = schedule.Session(1, 75, 5, 'Intro to CS 1', 'Roundtable', 'CS', 'Social Event', ['ASU'],['Mic'], [1])
+session2 = schedule.Session(2, 75, 5, 'Intro to CS 2', 'Lecture', 'CS', 'Social Event', ['ASU'], ['Mic', 'Wifi'], [1])
+session3 = schedule.Session(3, 75, 5, 'Intro to BS 1', 'Roundtable', 'BS', 'Social Event', ['Fulton'], ['Mic'], [2])
+session4 = schedule.Session(4, 75, 5, 'Intro to BS 2', 'Lecture', 'BS', 'Social Event', ['Fulton'], ['Wifi'], [2])
+session5 = schedule.Session(5, 75, 5, 'Intro to ENG 1', 'Lecture', 'BS', 'Social Event', ['Arts'], ['Wifi'], [2])
+session6 = schedule.Session(6, 75, 5, 'Intro to ENG 2', 'Lecture', 'BS', 'Social Event', ['Arts'], ['Wifi' ], [2])
 
 sessions = [session1, session2, session3, session4, session5, session6]
 rooms = [room1, room2]
 
+
 year = datetime.now().year
 month = datetime.now().month
-start_times = [datetime(year, month, 1, 7, 0), datetime(year, month, 1, 8, 30)]
-end_times = [datetime(year, month, 1, 8, 15), datetime(year, month, 1, 9, 45)]
+start_times = [datetime(1, 1, 1, 7, 0), datetime(1, 1, 1, 8, 30)]
+end_times = [datetime(1, 1, 1, 8, 15), datetime(1, 1, 1, 9, 45)]
 days = [datetime(year, month, 13), datetime(year, month, 14), datetime(year, month, 15), datetime(year, month, 16)]
+
 
 day_schedule = schedule.Schedule(start_times, end_times, sessions, rooms, days)
 day_schedule.init()
-selected_day_indexes = [2, 3]
-selected_sessions = sessions
-selected_rooms = rooms
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', sessions=sessions)
+    # Get sessions that match filters
+    filter_types = ['Social Event']
+    filter_formats = ['Roundtable', 'Lecture']
+    filter_sponsors = ['ASU', 'Fulton', 'Arts']
+    filter_topics = ['CS', 'BS']
+    results = day_schedule.get_filtered_sessions(filter_types, filter_formats, filter_sponsors, filter_topics)
+
+    return render_template('home.html', sessions=results)
 
 
 @app.route('/schedule')
 def create_schedule():
-    day_schedule.create_schedule(selected_sessions, selected_rooms, selected_day_indexes)
+    selected_sessions = sessions
+    selected_rooms = rooms
+    selected_days = days
+    selected_times = start_times
+
+    day_schedule.create_schedule(selected_sessions, selected_rooms, selected_days, selected_times)
     sessions_sched = day_schedule.get_scheduled_sessions()
+
+    day_schedule.print_schedule()
+
+    # Get rooms that match filters and number of available slots for the filtered days and slots
+    filter_days = [days[0], days[1], days[2], days[3]]
+    filter_times = [start_times[0], start_times[1]]
+    for results in day_schedule.get_filtered_room_availability(filter_days, filter_times, ['Mic', 'Wifi'], 100, ['Roundtable']):
+        print(f'Room {results[0].room_id} has {results[1]} slots')
 
     return render_template('schedule.html', schedule=sessions_sched)
 
--- schedule.py
+++ schedule.py
@@ -14,6 +14,7 @@ class Session:
     format: str                          # Format of session (e.g., roundtable)
     topic: str                           # Topic of session (e.g., "African History")
     type: str                            # Type of the session (e.g., Social Event)
+    sponsors: list[str]                  # List of sponsors, including co-sponsors
     equipment: list[str]                 # List of equipment needed (e.g., WiFi)
     speaker: list[int]                   # List of speaker ID's
     assigned_room: int = 0               # Room ID that the session is scheduled into
@@ -40,6 +41,9 @@ class Session:
 class Room:
     room_id: int                                                    # Unique room indentifier
     max_capacity: int                                               # Maximum number of people allowed
+    name: str                                                       # Name of the room
+    property: str                                                   # Property the room is located in
+    floor: int                                                      # Floor number the room is on
     format: str = ""                                                # Format of room (e.g., roundtable)
     equipment: list[str] = field(default_factory=list)              # List of equipment needed (e.g., WiFi)
     schedule: list[list[Session]] = field(default_factory=list)     # List of session lists that represent daily schedules
@@ -47,8 +51,15 @@ class Room:
 
 
     # Create a blank schedule with slots in between ROOM_START and ROOM_END 
-    def schedule_init(self, num_slots: int):         
-        self.schedule += [['_'] * num_slots]
+    def schedule_init(self, num_slots: int, start_times: list[datetime], end_times: list[datetime], day: datetime):   
+        empty_list = []
+
+        for slot_index in range(num_slots):
+            empty_slot = Session(-1, 0, 0, 'Emtpy', 'Empty', 'Empty', 'Empty', [], [], [])
+            empty_slot.set_time(start_times[slot_index], end_times[slot_index], day)
+            empty_list.append(empty_slot)
+
+        self.schedule += [empty_list]
         self.slots = num_slots
 
 
@@ -80,24 +91,26 @@ class Room:
 
 
     # Add the session to the specified day's schedule
-    def add_session(self, session: Session, day_index: int, day: datetime, start_times: list[datetime], end_times: list[datetime], speaker_log, topic_log) -> bool:
+    def add_session(self, session: Session, day_index: int, day: datetime, slots: list[int], start_times: list[datetime], end_times: list[datetime], speaker_log, topic_log, sponsor_log) -> bool:
         # Check if the session and room are compatible
         if not self.check_compatible(session):
             return False
 
         sched = self.schedule[day_index]
 
-        for i in range(len(sched)):
+        for i in slots:
             is_valid = True
             slot_duration = (end_times[i] - start_times[i]).total_seconds() / 60.0
 
-            if sched[i] != '_':                                             # Check if the schedule at this index already has a session
+            if sched[i].session_id != -1:                                         # Check if the schedule at this index already has a session
                 continue
-            elif session.duration > slot_duration:                          # Check if session duration exceeds slot duration
+            elif session.duration > slot_duration:                                # Check if session duration exceeds slot duration
                 continue
             elif set(speaker_log[day_index][i]).intersection(session.speaker):    # Check if there is a speaker conflict
                 continue
             elif session.topic in topic_log[day_index][i]:                        # Check if there is a topic conflict
+                continue
+            elif set(sponsor_log[day_index][i]).intersection(session.sponsors):   # Check if there is a sponsor conflict
                 continue
 
             # Insert the session if there is enough open space
@@ -108,6 +121,7 @@ class Room:
             # Update speaker and topic logs
             speaker_log[day_index][i] = speaker_log[day_index][i] + session.speaker
             topic_log[day_index][i] = topic_log[day_index][i] + [session.topic]
+            sponsor_log[day_index][i] = sponsor_log[day_index][i] + session.sponsors
             return True
 
         return False
@@ -118,11 +132,11 @@ class Room:
         print(f'Room {self.room_id} Schedule')
         print(f'   Equipment: {self.equipment}')
         print(f'   Format: {self.format}')
-        for i in range(len(self.schedule)):
+        for i in range(len(days)):
             print(f'\n   Day {days[i].date()}:')
             for j in range(len(self.schedule[i])):
-                if(self.schedule[i][j] == '_'):
-                    print(f'   {start_times[j].time()} - {end_times[j].time()}: {self.schedule[i][j]}')
+                if(self.schedule[i][j].session_id == -1):
+                    print(f'   {start_times[j].time()} - {end_times[j].time()}:')
                 else:
                     print(f'   {start_times[j].time()} - {end_times[j].time()}: {self.schedule[i][j].session_id}')
         print()
@@ -135,23 +149,25 @@ class Room:
 # it for another day.
 @dataclass
 class Schedule:
-    start_times: list[datetime]                                       # List of schedule interval start times
-    end_times: list[datetime]                                         # List of schedule interval end times
-    all_sessions: list[Session]                                       # List of all sessions
-    all_rooms: list[Room]                                             # List of all rooms
-    days: list[datetime]                                              # List of all days
-    days_scheduled: int = 0                                           # Number of days scheduled
-    rooms_sched: dict[int, Room] = field(default_factory=dict)        # Maps room ID's to rooms
+    start_times: list[datetime]                                           # List of schedule interval start times
+    end_times: list[datetime]                                             # List of schedule interval end times
+    all_sessions: list[Session]                                           # List of all sessions
+    all_rooms: list[Room]                                                 # List of all rooms
+    days: list[datetime]                                                  # List of all days
+    days_scheduled: int = 0                                               # Number of days scheduled
+    rooms_sched: dict[int, Room] = field(default_factory=dict)            # Maps room ID's to rooms
     sessions_scheduled: list[Session] = field(default_factory=list)       # List of scheduled sessions
     sessions_not_scheduled: list[Session] = field(default_factory=list)   # List of session not able to be scheduled
-    speaker_log: list[list[list[str]]] = field(default_factory=list)  # List of speakers in each time slot for each day
-    topic_log: list[list[list[str]]] = field(default_factory=list)    # List of topics in each time slot for each day
+    speaker_log: list[list[list[str]]] = field(default_factory=list)      # List of speakers in each time slot for each day
+    topic_log: list[list[list[str]]] = field(default_factory=list)        # List of topics in each time slot for each day
+    sponsor_log: list[list[list[str]]] = field(default_factory=list)     # List of sponsors and cosponsors in each time slot for each day
 
 
     # Create a blank log for speaker and topic logs
     def logs_init(self):
         self.topic_log += [[[]] * len(self.start_times)]
         self.speaker_log += [[[]] * len(self.start_times)]
+        self.sponsor_log += [[[]] * len(self.start_times)]
 
 
     def init(self):
@@ -161,7 +177,7 @@ class Schedule:
 
             # Initalize room schedules
             for room in self.all_rooms:
-                room.schedule_init(len(self.start_times))
+                room.schedule_init(len(self.start_times), self.start_times, self.end_times, self.days[i])
 
 
     # Print schedule
@@ -216,8 +232,79 @@ class Schedule:
         return topics
 
 
+    # Get index of slot in start_times list
+    def get_slot_index(self, time: datetime) -> int:
+        time_index = -1
+        
+        for i in range(len(self.start_times)):
+            if self.start_times[i].time() == time.time():
+                time_index = i
+        
+        return time_index
+
+
+    # Get index of days in days list
+    def get_day_index(self, day: datetime):
+        day_index = -1
+
+        for i in range(len(self.days)):
+            if self.days[i].date() == day.date():
+                day_index = i
+        
+        return day_index
+
+
+    # Get list of sessions that match filters
+    def get_filtered_sessions(self, types: list[str], formats: list[str], sponsors: list[str], topics: list[str]):
+        compatible_sessions = []
+        
+        for session in self.get_unscheduled_sessions():
+            if len(types) > 0 and session.type not in types:
+                continue
+            elif len(formats) > 0 and session.format not in formats:
+                continue
+            elif len(sponsors) > 0 and not set(session.sponsors).issubset(sponsors):
+                continue
+            elif len(topics) > 0 and session.topic not in topics:
+                continue
+
+            compatible_sessions.append(session)
+
+        return compatible_sessions
+
+
+
+    # Get list of rooms that match filters and the number of available slots of specified days and times
+    def get_filtered_room_availability(self, days: list[datetime], times: list[datetime], equipment: list[str], capacity: int, formats: list[str]) -> list[tuple()]:
+        compatible_rooms = []
+        
+        for room in self.all_rooms:
+            num_available = 0
+
+            for day in days:
+                day_index = self.get_day_index(day)
+
+                for time in times:
+                    slot_index = self.get_slot_index(time)
+
+                    if len(equipment) > 0 and not set(room.equipment).issubset(equipment):      # Check if this room's equipment is a subset of the filtered equipment
+                        continue
+                    elif room.max_capacity > capacity:                                          # Check if rooms capacity exceeds the filtered capacity
+                        continue
+                    elif len(formats) > 0 and not room.format in formats:                       # Check if the rooms format is in the list of filtered formats
+                        continue
+
+                    if room.schedule[day_index][slot_index].session_id == -1:
+                        num_available += 1
+
+            if num_available != 0:
+                compatible_rooms.append((room, num_available))
+
+        return compatible_rooms
+
+
     # Create a schedule for one day. Intended to be called once each day
-    def create_day_schedule(self, sessions: list[Session], rooms: list[Room], day_index: int, day: datetime):
+    def create_day_schedule(self, sessions: list[Session], rooms: list[Room], day_index: int, day: datetime, slots: list[datetime]):
         self.days_scheduled += 1
         self.sessions_not_scheduled = []
 
@@ -231,7 +318,7 @@ class Schedule:
                     room.set_format(sess.format)
                     self.rooms_sched[room.room_id] = room
 
-                if self.rooms_sched[room.room_id].add_session(sess, day_index, day, self.start_times, self.end_times, self.speaker_log, self.topic_log):
+                if self.rooms_sched[room.room_id].add_session(sess, day_index, day, slots, self.start_times, self.end_times, self.speaker_log, self.topic_log, self.sponsor_log):
                     is_scheduled = True
                     self.sessions_scheduled.append(sess)
                     break
@@ -241,12 +328,17 @@ class Schedule:
 
 
     # Create multiple day schedules. Requires a list of indexes matching the selected indexes of the days list.
-    def create_schedule(self, sessions: list[Session], rooms: list[Room], day_index_list: list[int]):
+    def create_schedule(self, sessions: list[Session], rooms: list[Room], days: list[datetime], times: list[datetime]):
         session_list = sessions
+        slot_indexes = []
         i = 0
 
-        while i < len(day_index_list) and len(session_list) > 0:
-            day_index = day_index_list[i]
-            self.create_day_schedule(session_list, rooms, day_index, self.days[day_index])
+        for time in times:
+            slot_indexes.append(self.get_slot_index(time))
+
+        while i < len(days) and len(session_list) > 0:
+            day_index = self.get_day_index(days[i])
+
+            self.create_day_schedule(session_list, rooms, day_index, days[i], slot_indexes)
             session_list = self.sessions_not_scheduled
             i += 1
